LineageOS/android_device_lge_d851

d851: Update for early-mounted system partition
 * This removes /system entry from fstab, since this partition
   is now early-mounted and the fstab entry is specified in device tree.

 * At the same time, also include a fully populated fstab, in order to avoid
   build breakage while generating recovery updater scripts.

Change-Id: I8e558458b984e97a45a920c3751a6b7c3f2ebc5c

BoardConfig.mk
@@ -41,7 +41,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 27325360128
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/lge/d851/rootdir/etc/fstab.g3
+TARGET_RECOVERY_FSTAB := device/lge/d851/rootdir/etc/fstab.recovery
 
 # RIL
 BOARD_GLOBAL_CFLAGS += -DUSE_RIL_VERSION_10

TheNoFace/android_device_lge_f400

BoardConfig.mk
@@ -42,7 +42,7 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 27426023424
 TARGET_USERIMAGES_USE_F2FS := true
 
 # Recovery
-TARGET_RECOVERY_FSTAB := device/lge/f400/rootdir/etc/fstab.g3
+TARGET_RECOVERY_FSTAB := device/lge/f400/rootdir/etc/fstab.recovery
 
 # RIL
 BOARD_GLOBAL_CFLAGS += -DUSE_RIL_VERSION_10